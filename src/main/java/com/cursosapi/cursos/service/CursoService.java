3fd51f5c477d3a098a359ee3872d5786bccf58c7
Joao Pedro <joaopedrogolenia@gmail.com>
2024-05-07 10:31:30 -0300

    feat(cursos): Adicionar endpoint de alteração de professor

--- src/main/java/com/cursosapi/cursos/service/CursoService.java
+++ src/main/java/com/cursosapi/cursos/service/CursoService.java
@@ -2,14 +2,29 @@ package com.cursosapi.cursos.service;
 
 import com.cursosapi.cursos.repository.CursoRepository;
 import com.cursosapi.cursos.entity.Curso;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public record CursoService(CursoRepository cursoRepository) {
+public class CursoService {
+
+    private final CursoRepository cursoRepository;
+
+    @Autowired
+    public CursoService(CursoRepository cursoRepository) {
+        this.cursoRepository = cursoRepository;
+    }
 
     public Curso cadastrar(Curso curso){
         return cursoRepository.save(curso);
     }
 
-
+    @Transactional
+    public Curso alterarProfessor(Long idCurso, String novoNome) {
+        Curso curso = cursoRepository.findById(idCurso)
+                .orElseThrow(() -> new RuntimeException("Curso não encontrado com o ID: " + idCurso));
+        curso.setProfessor(novoNome);
+        return cursoRepository.save(curso);
+    }
 }
--- src/main/java/com/cursosapi/cursos/web/CursoController.java
+++ src/main/java/com/cursosapi/cursos/web/CursoController.java
@@ -2,17 +2,29 @@ package com.cursosapi.cursos.web;
 
 import com.cursosapi.cursos.service.CursoService;
 import com.cursosapi.cursos.entity.Curso;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/curso")
-public record CursoController(CursoService cursoService) {
+public class CursoController {
+
+    private final CursoService cursoService;
+
+    public CursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
 
     @PostMapping
     public Curso cadastrar(@RequestBody Curso curso){
         return cursoService.cadastrar(curso);
+    }
+
+    @PatchMapping("/{id}/alterar_professor")
+    public ResponseEntity<Curso> alterarProfessor(@PathVariable Long id,
+                                                  @RequestBody String novoNomeProfessor) {
+        Curso cursoAtualizado = cursoService.alterarProfessor(id, novoNomeProfessor);
+        return new ResponseEntity<>(cursoAtualizado, HttpStatus.OK);
     }
 }
